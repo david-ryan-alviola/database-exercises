66078da1b7a52cfefecc327d402014960b112f0c
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2020-12-17 13:53:05 -0600

    Added aliases to make results more clear.

--- select_exercises.sql
+++ select_exercises.sql
@@ -8,18 +8,35 @@ SHOW TABLES;
 DESCRIBE albums_db.albums;
 
 # Name of all albums by Pink Floyd
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT name AS 'Pink Floyd Albums'
+	FROM albums
+	WHERE artist = 'Pink Floyd';
 
 # The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date AS 'Year Sgt. Pepper''s Lonely Hearts Club Band Released'
+	FROM albums
+	WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 # The genre for Nevermind
-SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT genre AS 'Genre for Nevermind'
+	FROM albums
+	WHERE name = 'Nevermind';
 
+# Albums that were released in the 90s
+SELECT name AS 'Albums released in 1990s'
+	FROM albums
+	WHERE release_date BETWEEN 1990 AND 1999;
+	
 # Which albums had less than 20 million certified sales
-SELECT name FROM albums WHERE sales < 20000000;
+SELECT name AS 'Albums with less than 20 million sales' 
+	FROM albums
+	WHERE sales < 20000000;
 
 # All the albums with a genre of "Rock"
-SELECT name FROM albums WHERE genre = 'Rock';
+SELECT name AS 'Only rock albums'
+	FROM albums
+	WHERE genre = 'Rock';
 # To include all types of rock
-SELECT name FROM albums WHERE genre LIKE '%rock%';
+SELECT name AS 'All types of rock albums'
+	FROM albums
+	WHERE genre LIKE '%rock%';
