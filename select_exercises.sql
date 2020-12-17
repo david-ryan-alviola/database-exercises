754ae49615a854fa4098dc5e6ad0d947870efa35
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2020-12-17 11:38:37 -0600

    Formatted SELECT statements

--- select_exercises.sql
+++ select_exercises.sql
@@ -8,18 +8,30 @@ SHOW TABLES;
 DESCRIBE albums_db.albums;
 
 # Name of all albums by Pink Floyd
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT name
+    FROM albums
+    WHERE artist = 'Pink Floyd';
 
 # The year Sgt. Pepper's Lonely Hearts Club Band was released
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT release_date
+    FROM albums
+    WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 # The genre for Nevermind
-SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT genre
+    FROM albums
+    WHERE name = 'Nevermind';
 
 # Which albums had less than 20 million certified sales
-SELECT name FROM albums WHERE sales < 20000000;
+SELECT name
+    FROM albums
+    WHERE sales < 20000000;
 
 # All the albums with a genre of "Rock"
-SELECT name FROM albums WHERE genre = 'Rock';
+SELECT name
+    FROM albums
+    WHERE genre = 'Rock';
 # To include all types of rock
-SELECT name FROM albums WHERE genre LIKE '%rock';
+SELECT name
+    FROM albums
+    WHERE genre LIKE '%rock';
