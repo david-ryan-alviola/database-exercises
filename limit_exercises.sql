695ec597a0be33695d4c7dc4593a5e7759c7472b
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2020-12-18 11:40:26 -0600

    Added names from question 2

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -6,6 +6,7 @@ SELECT DISTINCT last_name
 	FROM employees
 	ORDER BY last_name DESC
 	LIMIT 10;
+# Zykh, Zyda, Zwicker, Zweizig, Zumaque, Zultner, Zucker, Zuberek, Zschoche, Zongker
 	
 # 3. Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
 SELECT first_name, last_name, hire_date
