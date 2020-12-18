2817e281c69c519948f6f016a4411c75f4c41349
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2020-12-18 11:23:18 -0600

    Added hire_date to selection to verify correct sorting

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -8,7 +8,7 @@ SELECT DISTINCT last_name
 	LIMIT 10;
 	
 # 3. Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. Write a comment in your code that lists the five names of the employees returned.
-SELECT first_name, last_name
+SELECT first_name, last_name, hire_date
 	FROM employees
 	WHERE hire_date LIKE '199%'
 		AND birth_date LIKE '%-12-25'
@@ -17,7 +17,7 @@ SELECT first_name, last_name
 # Alselm Cappello, Utz Mandell, Bouchung Schreiter, Baocai Kushner, Petter Stroustrup
 
 # 4. Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results. LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
-SELECT first_name, last_name
+SELECT first_name, last_name, hire_date
 	FROM employees
 	WHERE hire_date LIKE '199%'
 		AND birth_date LIKE '%-12-25'
